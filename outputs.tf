e03ebd3ec53e6bdfb1cf9179e083c898c1422a41
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:06 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,45 +1,40 @@
-output "kubernetes_flux_configurations" {
-  description = "All kubernetes_flux_configuration resources"
-  value       = azurerm_kubernetes_flux_configuration.kubernetes_flux_configurations
-  sensitive   = true
-}
 output "kubernetes_flux_configurations_blob_storage" {
-  description = "List of blob_storage values across all kubernetes_flux_configurations"
-  value       = [for k, v in azurerm_kubernetes_flux_configuration.kubernetes_flux_configurations : v.blob_storage]
+  description = "Map of blob_storage values across all kubernetes_flux_configurations, keyed the same as var.kubernetes_flux_configurations"
+  value       = { for k, v in azurerm_kubernetes_flux_configuration.kubernetes_flux_configurations : k => v.blob_storage }
   sensitive   = true
 }
 output "kubernetes_flux_configurations_bucket" {
-  description = "List of bucket values across all kubernetes_flux_configurations"
-  value       = [for k, v in azurerm_kubernetes_flux_configuration.kubernetes_flux_configurations : v.bucket]
+  description = "Map of bucket values across all kubernetes_flux_configurations, keyed the same as var.kubernetes_flux_configurations"
+  value       = { for k, v in azurerm_kubernetes_flux_configuration.kubernetes_flux_configurations : k => v.bucket }
   sensitive   = true
 }
 output "kubernetes_flux_configurations_cluster_id" {
-  description = "List of cluster_id values across all kubernetes_flux_configurations"
-  value       = [for k, v in azurerm_kubernetes_flux_configuration.kubernetes_flux_configurations : v.cluster_id]
+  description = "Map of cluster_id values across all kubernetes_flux_configurations, keyed the same as var.kubernetes_flux_configurations"
+  value       = { for k, v in azurerm_kubernetes_flux_configuration.kubernetes_flux_configurations : k => v.cluster_id }
 }
 output "kubernetes_flux_configurations_continuous_reconciliation_enabled" {
-  description = "List of continuous_reconciliation_enabled values across all kubernetes_flux_configurations"
-  value       = [for k, v in azurerm_kubernetes_flux_configuration.kubernetes_flux_configurations : v.continuous_reconciliation_enabled]
+  description = "Map of continuous_reconciliation_enabled values across all kubernetes_flux_configurations, keyed the same as var.kubernetes_flux_configurations"
+  value       = { for k, v in azurerm_kubernetes_flux_configuration.kubernetes_flux_configurations : k => v.continuous_reconciliation_enabled }
 }
 output "kubernetes_flux_configurations_git_repository" {
-  description = "List of git_repository values across all kubernetes_flux_configurations"
-  value       = [for k, v in azurerm_kubernetes_flux_configuration.kubernetes_flux_configurations : v.git_repository]
+  description = "Map of git_repository values across all kubernetes_flux_configurations, keyed the same as var.kubernetes_flux_configurations"
+  value       = { for k, v in azurerm_kubernetes_flux_configuration.kubernetes_flux_configurations : k => v.git_repository }
   sensitive   = true
 }
 output "kubernetes_flux_configurations_kustomizations" {
-  description = "List of kustomizations values across all kubernetes_flux_configurations"
-  value       = [for k, v in azurerm_kubernetes_flux_configuration.kubernetes_flux_configurations : v.kustomizations]
+  description = "Map of kustomizations values across all kubernetes_flux_configurations, keyed the same as var.kubernetes_flux_configurations"
+  value       = { for k, v in azurerm_kubernetes_flux_configuration.kubernetes_flux_configurations : k => v.kustomizations }
 }
 output "kubernetes_flux_configurations_name" {
-  description = "List of name values across all kubernetes_flux_configurations"
-  value       = [for k, v in azurerm_kubernetes_flux_configuration.kubernetes_flux_configurations : v.name]
+  description = "Map of name values across all kubernetes_flux_configurations, keyed the same as var.kubernetes_flux_configurations"
+  value       = { for k, v in azurerm_kubernetes_flux_configuration.kubernetes_flux_configurations : k => v.name }
 }
 output "kubernetes_flux_configurations_namespace" {
-  description = "List of namespace values across all kubernetes_flux_configurations"
-  value       = [for k, v in azurerm_kubernetes_flux_configuration.kubernetes_flux_configurations : v.namespace]
+  description = "Map of namespace values across all kubernetes_flux_configurations, keyed the same as var.kubernetes_flux_configurations"
+  value       = { for k, v in azurerm_kubernetes_flux_configuration.kubernetes_flux_configurations : k => v.namespace }
 }
 output "kubernetes_flux_configurations_scope" {
-  description = "List of scope values across all kubernetes_flux_configurations"
-  value       = [for k, v in azurerm_kubernetes_flux_configuration.kubernetes_flux_configurations : v.scope]
+  description = "Map of scope values across all kubernetes_flux_configurations, keyed the same as var.kubernetes_flux_configurations"
+  value       = { for k, v in azurerm_kubernetes_flux_configuration.kubernetes_flux_configurations : k => v.scope }
 }
 
